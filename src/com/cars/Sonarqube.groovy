a491e5d2e8acb273aea6aadd10a67b753d381699
Sérgio Serra <sergioserra99@gmail.com>
2017-07-26 11:58:23 +0100

    Modified sonarqube class, moved withSonarQubeEnv outside the class.

--- src/com/cars/Sonarqube.groovy
+++ src/com/cars/Sonarqube.groovy
@@ -11,23 +11,19 @@ class Sonarqube implements Serializable{
 
     def runPreviewAnalysisForPullRequest(pullRequestId) {
 
-      def scannerHome = tool 'SonarScanner';
-      //def pullRequestId = env.CHANGE_ID // Get pull request id
-      withSonarQubeEnv('SonarServer') {
-          script.sh "$scannerHome/bin/sonar-scanner " +
-                  '-Dsonar.projectName=Android-Cars ' +
-                  '-Dsonar.projectKey=android-cars ' +
-                  '-Dsonar.host.url=http://localhost:9000 ' +
-                  '-Dsonar.login=admin ' +
-                  '-Dsonar.password=admin ' +
-                  '-Dsonar.language=java ' +
-                  '-Dsonar.sources=app/src/main/java ' +
-                  '-Dsonar.github.repository=AtlasVerticals/android-cars ' +
-                  '-Dsonar.github.oauth=098fbdbc659876f2e63e370bcff2ab00958bd14e ' +
-                  '-Dsonar.analysis.mode=preview ' +
-                  "-Dsonar.github.pullRequest=$pullRequestId"
-
-      }
+      def scannerHome = script.tool 'SonarScanner';
+      script.sh "$scannerHome/bin/sonar-scanner " +
+              '-Dsonar.projectName=Android-Cars ' +
+              '-Dsonar.projectKey=android-cars ' +
+              '-Dsonar.host.url=http://localhost:9000 ' +
+              '-Dsonar.login=admin ' +
+              '-Dsonar.password=admin ' +
+              '-Dsonar.language=java ' +
+              '-Dsonar.sources=app/src/main/java ' +
+              '-Dsonar.github.repository=AtlasVerticals/android-cars ' +
+              '-Dsonar.github.oauth=098fbdbc659876f2e63e370bcff2ab00958bd14e ' +
+              '-Dsonar.analysis.mode=preview ' +
+              "-Dsonar.github.pullRequest=$pullRequestId"
 
     }
 
