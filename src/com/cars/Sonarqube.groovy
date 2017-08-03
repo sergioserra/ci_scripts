b29607a20582f949aaca8098566b5e709a056f75
Sérgio Serra <sergioserra99@gmail.com>
2017-08-03 16:54:01 +0100

    Remove unused code.

--- src/com/cars/Sonarqube.groovy
+++ src/com/cars/Sonarqube.groovy
@@ -10,41 +10,11 @@ class Sonarqube implements Serializable{
     }
 
     def runPreviewAnalysisForPullRequest(pullRequestId) {
-
       script.sh './gradlew sonarqube -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest='+5
-
-      /*def scannerHome = script.tool "SonarScanner"
-      script.sh "$scannerHome/bin/sonar-scanner " +
-          "-Dsonar.projectName=Android-Cars " +
-          "-Dsonar.projectKey=android-cars " +
-          "-Dsonar.host.url=${script.env.sonarqube} " +
-          "-Dsonar.login=${script.env.login} " +
-          "-Dsonar.password=${script.env.password} " +
-          "-Dsonar.language=java " +
-          "-Dsonar.sources=app/src/main/java " +
-          "-Dsonar.github.repository=${script.env.repository} " +
-          "-Dsonar.github.oauth=${script.env.oauth} " +
-          "-Dsonar.analysis.mode=preview " +
-          "-Dsonar.github.pullRequest=$pullRequestId" */
-
     }
 
     def runFullAnalysis() {
-
       script.sh './gradlew sonarqube'
-
-      /*def scannerHome = tool "SonarScanner"
-      script.sh "$scannerHome/bin/sonar-scanner " +
-          "-Dsonar.projectName=Android-Cars " +
-          "-Dsonar.projectKey=android-cars " +
-          "-Dsonar.host.url=${script.env.sonarqube} " +
-          "-Dsonar.login=${script.env.login} " +
-          "-Dsonar.password=${script.env.password} " +
-          "-Dsonar.language=java " +
-          "-Dsonar.sources=app/src/main/java " +
-          "-Dsonar.github.repository=${script.env.repository} " +
-          "-Dsonar.github.oauth=${script.env.oauth} "*/
-
     }
 
 }
